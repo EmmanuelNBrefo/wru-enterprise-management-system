8213eef4fc0d9d6ad1330d94b8e1b9b03b203144
Emmanuel N. Brefo <nyuah2004@gmail.com>
2026-07-23 23:33:38 +0100

    feat(admin): customize WRU-EMS administration branding

---
+++ config/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+
+admin.site.site_header = (
+    "WESTERN REGION UNIVERSITY - WRU-EMS"
+)
+
+admin.site.site_title = (
+    "WRU-EMS | Western Region University"
+)
+
+admin.site.index_title = (
+    "Enterprise Management System Dashboard"
+)
